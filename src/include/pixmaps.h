bd151dd1bdbb80c977a6c6a6a48289a8d59f6ee0
David Freese <iam_w1hkj@w1hkj.com>
2015-03-23 07:52:24 -0500

    FSF address
      * correct FSF address in various files

--- src/include/pixmaps.h
+++ src/include/pixmaps.h
@@ -11,10 +11,11 @@
 // copyright under the GNU General Public License.
 //
 // You should have received a copy of the GNU General Public License
-// along with the program; if not, write to the Free Software
-// Foundation, Inc.
-// 59 Temple Place, Suite 330
-// Boston, MA  02111-1307 USA
+// along with the program; if not, write to the
+//
+//  Free Software Foundation, Inc.
+//  51 Franklin Street, Fifth Floor
+//  Boston, MA  02110-1301 USA.
 //
 // =====================================================================
 
